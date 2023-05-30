cbdb820f4c816d3f629ac6db080b42622111758f
samuelwolde1 <126281252+samuelwolde1@users.noreply.github.com>
2023-05-30 11:11:59 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Dice Roll Simulator By Samuel Wolde
 
+import random
 
 #Main Program Loop
 loop = True
@@ -12,43 +13,51 @@ while loop:
     print("4: Roll Dice until Snake Eyes")
     print("5: Exit")
 
-    #Choose a random number 1-6 and get the sum of the two random numbers
-    import random
-    
-
     # Get Menu Selection from User
     selection = input("Select an option (1-5): ")
 
     # Take Action Based on Menu Selection 
     if selection == "1":
-        rand_num = random.randrange(1, 7)
-        sum = rand_num + rand_num
-        print(str(rand_num) + "," + str(rand_num) + " (sum: " + str(sum) + ")")
+        rand_num1 = random.randrange(1, 7)
+        rand_num2 = random.randrange(1, 7)
+        sum = rand_num1 + rand_num2
+        print(str(rand_num1) + "," + str(rand_num2) + " (sum: " + str(sum) + ")")
     elif selection == "2":
         loop2 = True
         rolls = 0
         while loop2:
-            rolls+=1
+            rolls += 1
             rand_num1 = random.randrange(1, 7)
-            sum1 = rand_num1 + rand_num
-            print(str(rand_num) + "," + str(rand_num) + " (sum: " + str(sum) + ")")
+            rand_num2 = random.randrange(1, 7)
+            sum = rand_num1 + rand_num2
+            print(str(rand_num1) + "," + str(rand_num2) + " (sum: " + str(sum) + ")")
             if rolls == 5:
                 loop2 = False
 
     elif selection == "3":
-        rand_num1 = random.randrange(1, 7)
-        rand_num2 = random.randrange(1, 7)
-        sum = rand_num1 + rand_num2
-        rolls_requested = input("\nHow many rolls would you like? ")
         rolls = 0
         loop3 = True
+        rolls_requested = input("\nHow many rolls would you like? ")
         while loop3:
+            rand_num1 = random.randrange(1, 7)
+            rand_num2 = random.randrange(1, 7)
+            sum = rand_num1 + rand_num2
             rolls+= 1
             print(str(rand_num1) + "," + str(rand_num2) + " (sum: " + str(sum) + ")")
             if rolls == int(rolls_requested):
                 loop3 = False
     elif selection == "4":
-        print("\noption 4")
+        rolls = 0
+        loop4 = True
+        while loop4:
+            rand_num1 = random.randrange(1, 7)
+            rand_num2 = random.randrange(1, 7)
+            sum = rand_num1 + rand_num2
+            rolls += 1
+            print(str(rand_num1) + "," + str(rand_num2) + " (sum: " + str(sum) + ")")
+            if rand_num1 == rand_num2:
+                print("\nSNAKE EYES! It took " + str(rolls) + " rolls to get snake eyes.")
+                loop4 = False
     elif selection == "5":
         print("\nEXIT")
         loop = False
